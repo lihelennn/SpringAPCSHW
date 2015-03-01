8831de6d40ed442df2d1e0a87743ce88a2a5a494
lihelennn <helen82198@gmail.com>
2015-03-01 15:49:33 -0500

    fixed most errors

--- 04USACO/makelake.java
+++ 04USACO/makelake.java
@@ -29,6 +29,7 @@ public class makelake{
 	    Scanner scan = new Scanner(new File(fileName));
 	    lake = new int [scan.nextInt()][scan.nextInt()];
 	    landElevation = scan.nextInt();
+	    // System.out.println("landElevation" + landElevation);
 	    instructions = new int [scan.nextInt()][3];
 	    for(int i = 0;i<lake.length;i++){
 		for(int ii=0;ii<lake[0].length;ii++){
@@ -49,8 +50,6 @@ public class makelake{
 	for(int i = 0;i<instructions.length;i++){
 	    stomp(i);
 	}
-	System.out.println("stomp");
-	System.out.println(this);
     }
 
     //stomp() helper method
@@ -59,9 +58,9 @@ public class makelake{
 	int y = instructions[step][1];
 	int b = findBiggest(x,y);
 	int limit = b-instructions[step][2];
-	System.out.println(limit);
-	for(int i=0;i<3;i++){
-	    for(int m=0;m<3;m++){
+	//	System.out.println(limit);
+	for(int i=-1;i<2;i++){
+	    for(int m=-1;m<2;m++){
 		try{
 		    if(lake[x+i][y+m]>limit){
 			lake[x+i][y+m] = limit;
@@ -69,13 +68,14 @@ public class makelake{
 		}catch(Exception IndexOutOfBoundsException){}
 	    }
 	}
-	System.out.println(this);
+	// System.out.println("stomp" + step);
+	// System.out.println(this);
     }
     //stomp(int step) helper method
     private int findBiggest(int x, int y){
 	int b = 0;
-	for(int i=0;i<3;i++){
-	    for(int ii=0;ii<=3;ii++){
+	for(int i=-1;i<2;i++){	   
+	    for(int ii=-1;ii<2;ii++){
 		try{
 		    if(lake[x+i][y+ii]>b){
 			b = lake[x+i][y+ii];
@@ -83,35 +83,40 @@ public class makelake{
 		}catch(Exception IndexOutOfBoundsException){}
 	    }
 	}
+	//	System.out.println("biggest" + b);
 	return b;
     }
 
 
     private void calcDepth(){
+	//	System.out.println("landElevation" + landElevation);
 	for(int i=0;i<lake.length;i++){
-	    for(int ii=0;ii<lake.length;ii++){
+	    //	    System.out.println(i);
+	    //	    System.out.println(this);
+	    for(int ii=0;ii<lake[0].length;ii++){
 		if(lake[i][ii]<landElevation){
 		    lake[i][ii]=landElevation-lake[i][ii];;
 		}else{
 		    lake[i][ii]=0;
+		    // System.out.println(lake[i][ii]);
 		}
 	    }
 	}
-	System.out.println(this);
+	// System.out.println("calcDepth");
+       	// System.out.println(this);
     }
 
     private int calcVolume(){
 	int t=0;
 	int c = 0;
 	for(int i=0;i<lake.length;i++){
-	    for(int ii=0;ii<lake.length;ii++){
-		if(lake[i][ii]>0){
-		    c = lake[i][ii];
-		    t=t+c;
-		}
+	    for(int ii=0;ii<lake[0].length;ii++){
+		t+=lake[i][ii];
 	    }
 	}
-	return t*72*72;
+	// System.out.println(t);
+	//	return t*72*72;
+	return t*(t+6)*(t+6);
     }
 
     public int runProgram(){
@@ -122,6 +127,7 @@ public class makelake{
 
     public static void main(String[]moo){
 	makelake beef = new makelake("testing.txt");
+	// System.out.println(beef);
 	System.out.println(beef.runProgram());
     }
 
