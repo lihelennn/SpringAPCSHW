71633eb35b5cfa65818149b32178531ef7c59f89
lihelennn <helen82198@gmail.com>
2015-03-01 16:40:08 -0500

    made constructor

--- 04USACO/ctravel.java
+++ 04USACO/ctravel.java
@@ -5,18 +5,20 @@ import java.io.*;
 
 public class ctravel{
 
-    private int[][]field;
-    private int[][]destination=new int[2][2];
+    private char[][]field;
+    private int[][]destination = new int[2][2];
     private int time;
+    String help = "";
   
-    public cowTravel(String fileName){
+    public ctravel(String fileName){
 	try{
 	    Scanner scan = new Scanner(new File(fileName));
-	    field = new int[scan.nextInt()][scan.nextInt()];
+	    field = new char[scan.nextInt()][scan.nextInt()];
 	    time = scan.nextInt();
 	    for(int i=0;i<field.length;i++){
+		help = scan.next();
 		for(int ii=0;ii<field[0].length;ii++){
-		    field[i][ii]=scan.nextInt();
+		    field[i][ii]=help.charAt(ii);
 		}
 	    }
 	    for(int i=0;i<2;i++){
@@ -28,5 +30,37 @@ public class ctravel{
 	    System.out.println("File not found");
 	}
     }
+
+    public String toString(){
+	int place = 0;
+	int place2 = 0;
+	String ans = "";
+	while (place < field.length){
+	    while (place2 < field[0].length){
+		ans += field[place][place2] + "   ";
+		place2 ++;
+	    }
+	    place2 = 0;
+	    place ++;
+	    ans += "\n";
+	}
+	return ans;
+    }
+
+
+    public boolean solve(int x, int y, int seconds){
+	if (field[x][y] == '*'){
+	    return false;
+	}
+	return false;
+
+    }
+
+    public static void main(String[] args){
+	ctravel test = new ctravel("testingctravel.txt");
+	System.out.println(test);
+    }
+
+
 
 }
