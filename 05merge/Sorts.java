bb7e4ca077203f89686a651d4e50943821b0ef75
lihelennn <helen82198@gmail.com>
2015-03-02 21:56:17 -0500

    not there yet

--- 05merge/Sorts.java
+++ 05merge/Sorts.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Sorts{
 
-    public static void merge(int[]a, int[]b){
+    public static void merge(int[]a, int[]b, int[]c){
 	int placea = 0;
 	int placeb = 0;
 	int placeans = 0;
@@ -29,28 +29,43 @@ public class Sorts{
 	    placeb++;
 	    placeans++;
 	}
-	a = Arrays.copyOf(ans);
+	System.arraycopy(ans,0,c,0,ans.length);
 	/*
-	int place = 0;
-	String answerString = "[";
-	while (place < ans.length){
-	    answerString += ans[place] + ", ";
-	    place++;
-	} 
-	return answerString;
+	  int place = 0;
+	  String answerString = "[";
+	  while (place < ans.length){
+	  answerString += ans[place] + ", ";
+	  place++;
+	  } 
+	  return answerString;
 	*/
     }
 
 
     public static void mergesort(int[]hello){
+	System.out.println(Arrays.toString(hello));
 	int half = hello.length/2;
 	int otherhalf = hello.length - half;
 	int[]one = new int[half];
 	int[]two = new int [otherhalf];
+	int[]combine = new int[hello.length];
+
+	if (one.length <= 1 && two.length <= 1){
+	    merge(one, two, combine);
+	    System.arraycopy(combine,0,hello,0,one.length+two.length);
+	}
 
 
 
+	while (hello.length > 2){
+	    mergesort(one);
+	    mergesort(two);
+	    merge(one,two,hello);
+	}
 
+
+
+    }
 
 
 
@@ -59,16 +74,18 @@ public class Sorts{
 
 
     public static void main(String[]args){
-	int[]test1 = new int[3];
-	int[]test2 = new int[4];
-	test1[0] = 1;
-	test1[1] = 3;
-	test1[2] = 5;
-	test2[0] = 2;
-	test2[1] = 4;
-	test2[2] = 6;
-	test2[3] = 8;
-	merge(test1, test2);
+	int[]test1 = new int[7];
+	// int[]test2 = new int[4];
+	// int[]c = new int[test1.length + test2.length];
+	test1[0] = 10;
+	test1[1] = 7;
+	test1[2] = 4;
+	test1[3] = 2;
+	test1[4] = 9;
+	test1[5] = 6;
+	test1[6] = 8;
+	mergesort(test1);
+	// System.out.println(Arrays.toString(c));
     }
 }
 			   
