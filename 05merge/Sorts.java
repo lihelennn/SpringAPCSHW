58aad20b6b98ca3dceb18ace9129a54088ddec52
lihelennn <helen82198@gmail.com>
2015-03-03 23:08:57 -0500

    tested with times and certain arrays; all good!

--- 05merge/Sorts.java
+++ 05merge/Sorts.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class Sorts{
 
+    //sorted: 100,000 elements 78 milliseconds
+    //reverse sorted: 100,000 elements 31 milliseconds
+    //small range random (1, 2, 3): 100,000 elements 15 milliseconds
+    //large range random (-1000000 - 1000000): 100,000 elements 32 milliseconds
+
     public static void merge(int[]a, int[]b, int[]c){
 	int placea = 0;
 	int placeb = 0;
@@ -45,6 +50,8 @@ public class Sorts{
 
     public static void mergesort(int[]hello){
 	// System.out.println(Arrays.toString());
+
+	//split
 	int half = hello.length/2;
 	int otherhalf = hello.length - half;
 	int[]one = new int[half];
@@ -62,41 +69,94 @@ public class Sorts{
 	    place++;
 	}
 	// System.out.println(Arrays.toString(one));
-
 	// System.out.println(Arrays.toString(two));
 	// System.arraycopy(hello,one.length,two,0,two.length);
 
 	
 	int[]combine = new int[hello.length];
 
+	//divide
 	if (hello.length >= 2){
 	    mergesort(one);
 	    mergesort(two);
 	}
 
+	//merge
 	merge(one, two, combine);
 	// System.out.println(1);
 	System.arraycopy(combine,0,hello,0,combine.length);
 	// System.out.println(Arrays.toString(hello));
 
-	
-
     }
+
+    /*
 
     public static void main(String[]args){
-	int[]test1 = new int[7];
+
+	Random r = new Random();
+	int[]test1 = new int[100000];
+	int start = 0;
+	while (start < test1.length){
+	    test1[start]=start;
+	    start++;
+	}
+	int[]test2 = new int[100000];
+	start = 0;
+	int end = 100000;
+	while (start < test2.length){
+	    test2[start] = end;
+	    start++;
+	    end--;
+	}
+	int[]test3 = new int[100000];
+	int place = 0;
+	while (place < test3.length){
+	    test3[place] = r.nextInt(3) + 1;
+	    place++;
+	}
+	int[]test4 = new int[100000];
+	place = 0;
+	while (place < test4.length){
+	    test4[place] = r.nextInt(2000000) - 1000000;
+	    place++;
+	}
+
 	// int[]test2 = new int[4];
 	// int[]c = new int[test1.length + test2.length];
-	test1[0] = 10;
-	test1[1] = 7;
-	test1[2] = 4;
-	test1[3] = 2;
-	test1[4] = 9;
-	test1[5] = 6;
-	test1[6] = 8;
+
+	long start_time = System.currentTimeMillis();
 	mergesort(test1);
-	System.out.println(Arrays.toString(test1));
-	// System.out.println(Arrays.toString(c));
+	long end_time = System.currentTimeMillis();
+	System.out.println(end_time-start_time);
+	// System.out.println(test1);
+	// System.out.println(Arrays.toString(test1));
+
+	start_time = System.currentTimeMillis();
+	mergesort(test2);
+	end_time = System.currentTimeMillis();
+	System.out.println(end_time-start_time);
+	// System.out.println(test2);
+	// System.out.println(Arrays.toString(test2));
+
+	start_time = System.currentTimeMillis();
+	mergesort(test3);
+	end_time = System.currentTimeMillis();
+	System.out.println(end_time-start_time);
+	// System.out.println(test3);
+	// System.out.println(Arrays.toString(test3));
+
+	start_time = System.currentTimeMillis();
+	mergesort(test4);
+	end_time = System.currentTimeMillis();
+	System.out.println(end_time-start_time);
+	// System.out.println(test4);
+	// System.out.println(Arrays.toString(test4));
+
     }
+    
+    */
+
+
+   
 }
-			   
+			  
