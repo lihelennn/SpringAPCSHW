f286e73a0e5a50712324668e66ae43828f99ba31
Helen Li <helen82198@gmail.com>
2015-03-05 12:26:23 -0500

    starting quick select

---
+++ Quick.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Quick{
+
+    public void partition(int[]ary, int si, int ei){
+	int[]copy = new int[ary.length];
+	int place = 0;
+
+	//copy array over to a new copy
+	while (place < ary.length){
+	    if (place != 
+	    copy[place]=ary[place];
+	    place++;
+	}
+
