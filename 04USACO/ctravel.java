a0464ba7e6903cc28992997c34dfdb7a0601c963
lihelennn <helen82198@gmail.com>
2015-03-04 21:57:14 -0500

    fixed after seeing in class; should've return int

--- 04USACO/ctravel.java
+++ 04USACO/ctravel.java
@@ -57,74 +57,106 @@ public class ctravel{
 	}
     }
 
+    /*
+      Doesn't worked :'( I tried
 
-    public boolean solve(int x, int y, int current){
-	// System.out.println(this);
-	// wait(20);
-	// System.out.println(x + "," + y);
+      public boolean solve(int x, int y, int current){
+      // System.out.println(this);
+      // wait(20);
+      // System.out.println(x + "," + y);
 
-	if (x<0 || x>=field.length || y<0 || y>=field[0].length){
-	    return false;
-	}
-	if (field[x][y] == '*'){ 
-	    return false;
-	}
+      if (x<0 || x>=field.length || y<0 || y>=field[0].length){
+      return false;
+      }
+      if (field[x][y] == '*'){ 
+      return false;
+      }
+
+      if (timeremaining == 0){
+      return (x == destination[1][0] && y == destination[1][1]);
+      }
+      
+
+      if (x == destination[1][0] && y == destination[1][1]){
+      return true;
+      }
+
+      if (field[x][y] == '.'){
+      field[x][y] = '@';
+      if (solve(x+1, y, current+1) || solve(x-1, y, current+1) || solve(x, y+1, current+1) || solve(x, y-1, current+1)){
+      field[x][y] = (char)current;
+      return true;
+      }
+      field[x][y] = '*';
+      }
+      return false;
+      }
+
+      public int solver(int x, int y, int current, int solutions){
+      if (solve(x, y, 0)){
+      if (current == time && x == destination[1][0] && y == destination[1][1]){
+      return solutions;
+      }
+      if (x>=0 && x<field.length && y>=0 && y<field[0].length){
+      if (field[x][y] != '*'){
+      if (solve(x+1,y,current+1)){
+      solver(x+1,y,current+1,solutions+1);
+      }
+      if (solve(x-1,y,current+1)){
+      solver(x-1,y,current+1,solutions+1);
+      }
+      if (solve(x,y+1,current+1)){
+      solver(x,y+1,current+1,solutions+1)
+      }
+      }
+      }
+      }
+      }
+
+    */
+
+
+    public int solver(int x, int y, int remaining){
 
 	/*
-	if (timeremaining == 0){
-	    return (x == destination[1][0] && y == destination[1][1]);
-	}
+	  System.out.println("destination" + destination[1][0] + ',' + destination[1][1]);
+	  System.out.println(this);
+	  System.out.println(time);
 	*/
 
-	if (x == destination[1][0] && y == destination[1][1]){
-	    return true;
+	if (x<0 || x>=field.length || y<0 || y>=field[0].length){
+	    return 0;
 	}
 
-	if (field[x][y] == '.'){
-	    field[x][y] = '@';
-	    if (solve(x+1, y, current+1) || solve(x-1, y, current+1) || solve(x, y+1, current+1) || solve(x, y-1, current+1)){
-		field[x][y] = (char)current;
-		return true;
+	if (remaining == 0){
+	    if (x == destination[1][0] && y == destination[1][1]){
+		return 1;
+	    }else{
+		return 0;
 	    }
-	    field[x][y] = '*';
 	}
-	return false;
+
+	if (field[x][y] == '*'){
+	    return 0;
+	}
+	// field[x][y] = 'Q';
+	return solver(x+1,y,remaining-1) + solver(x-1,y,remaining-1) + solver(x,y+1,remaining-1) + solver(x,y-1,remaining-1);
     }
 
-    public int solver(int x, int y, int current, int solutions){
-	if (solve(x, y, 0)){
-	    if (current == time && x == destination[1][0] && y == destination[1][1]){
-		return solutions;
-	    }
-	    if (x>=0 && x<field.length && y>=0 && y<field[0].length){
-		if (field[x][y] != '*'){
-		    if (solve(x+1,y,current+1)){
-			solver(x+1,y,current+1,solutions+1);
-		    }
-		    if (solve(x-1,y,current+1)){
-			solver(x-1,y,current+1,solutions+1);
-		    }
-		    if (solve(x,y+1,current+1)){
-			solver(x,y+1,current+1,solutions+1)
-
-
-
-
-
-
-    public boolean run(){
+    public int run(){
 	System.out.println(destination[0][0] + "," + destination[0][1]);
-	return solve(destination[0][0], destination[0][1],0);
+	return solver(destination[0][0], destination[0][1],time);
     }
 
-    public static void main(String[] args){
-	ctravel test = new ctravel("testingctravel.txt");
-	System.out.println(test);
-	System.out.println(test.run());
-	System.out.println(test);
+    /*
+      public static void main(String[] args){
+      ctravel test = new ctravel("testingctravel3.txt");
+      System.out.println(test);
+      System.out.println(test.run());
+      System.out.println(test);
 
-    }
-
-
+      }
+    
+    */
 
 }
