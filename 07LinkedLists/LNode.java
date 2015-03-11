57f91c08ee788161e51244a64fdca04067f380ca
Helen Li <helen82198@gmail.com>
2015-03-11 12:25:45 -0400

    start

---
+++ 07LinkedLists/LNode.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class LNode{
+    private LNode next;
+    private int data;
+
+    public LNode(int n){
+	data = n;
+    }
+
+    // public LNode(){
+    // 	data=null;
+    // 	next = null;
+    // }
+
+    public LNode(int n, LNode m){
+	data = n;
+	next = m;
+    }
+
+    public int getData(){
+	return data;
+    }
+
+    public void setData(int n){
+	data = n;
+    }
+
+    public LNode getNext(){
+	return next;
+    }
+
+    public void setNext(LNode n){
+	next = n;
+    }
+
+    public int toString(){
+	return data;
+    }
+
+}
+
