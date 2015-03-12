12634544739307c04940bfc0d761ce29f23cfd52
Helen Li <helen82198@gmail.com>
2015-03-12 12:25:49 -0400

    more

--- 07LinkedLists/MyLinkedList.java
+++ 07LinkedLists/MyLinkedList.java
@@ -32,6 +32,17 @@ public class MyLinkedList{
 	return true;
     }
 
+    public int size(){
+	int ans = 0;
+	LNode temp = a;
+	while (temp.getNext() != null){
+	    ans++;
+	    temp = temp.getNext();
+	}
+
+    /*
     public int get(int index){
+	if (index < 0 || index >= 
+    */
 	
 }
