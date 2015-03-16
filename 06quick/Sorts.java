67f83f8163a08fbbe6299f03df2b446d1e0e9155
Helen Li <helen82198@gmail.com>
2015-03-16 12:23:40 -0400

    minor changes

--- 06quick/Sorts.java
+++ 06quick/Sorts.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class Sorts{
 
+    public String name(){
+	return "li.helen";
+    }
+
     public static int partition(int[]ary, int si, int ei){
 	int[]D = new int[ary.length];
 	int place = 0;
@@ -102,7 +106,7 @@ public class Sorts{
 	ary[ary.length-1] = pivotal;
 
 	while (start < end){
-	    // System.out.println(Arrays.toString(ary));
+	    System.out.println(Arrays.toString(ary));
 	  
 	    if (ary[start] > ary[end]){
 		one = ary[start];
@@ -138,7 +142,7 @@ public class Sorts{
     public static void quicksortH(int[]ary, int start, int end){
 	// int start = 0;
 	// int end = ary.length-1;
-	int now = partitionIn(ary,start,end);
+	int now = partitionIn2(ary,start,end);
 	// System.out.println(Arrays.toString(ary));
 	// System.out.println("start" + start);
 	// System.out.println("end" + end);
@@ -158,13 +162,13 @@ public class Sorts{
 	quicksortH(ary,0,ary.length-1);
     }
 
-    /*
+    
     public static void main(String[]args){
 	int[]ary = {3,10,2,-9,9,14,13,15,100,-100};
-	// System.out.println(Arrays.toString(ary));
+	System.out.println(Arrays.toString(ary));
 	quicksort(ary);
-	// System.out.println(Arrays.toString(ary));
+	System.out.println(Arrays.toString(ary));
     }
-    */
+    
 
 }
