accd98c598975a70f40b48ae71926a64555ce0ac
lihelennn <helen82198@gmail.com>
2015-03-16 20:32:41 -0400

    testing stuff

--- 06quick/Driver.java
+++ 06quick/Driver.java
@@ -5,6 +5,7 @@ public class Driver{
     public static void main(String[]args){
 	Sorts hi = new Sorts();
 	int[] tester = {100, -98, 45, 36, -92, 1000, 50, 80, 70};
+	System.out.println(hi.name());
 	System.out.println(Arrays.toString(tester));
 	System.out.println(Sorts.quickSelect(tester,3));
 	Sorts.quicksort(tester);
@@ -13,6 +14,7 @@ public class Driver{
 
 	quickSelect hello = new quickSelect();
 	int[]test = {45, 69, 50, -100, 1000, -400, 560, -479};
+	System.out.println(hello.name());
 	System.out.println(Arrays.toString(test));
 	// System.out.println(quickSelect.quickSelect(test,100));
 	Sorts.quicksort(test);
