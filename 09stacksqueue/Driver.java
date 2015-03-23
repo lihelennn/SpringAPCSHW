af62d56e6dd8e5f77b17c02c236cb5a4113c9224
lihelennn <helen82198@gmail.com>
2015-03-23 19:25:50 -0400

    tested

--- 09stacksqueue/Driver.java
+++ 09stacksqueue/Driver.java
@@ -15,8 +15,15 @@ public class Driver{
 	System.out.println(test1.dequeue());
 	System.out.println(test1);
 
-
-
+	MyStack<Integer> test2 = new MyStack<Integer>();
+	System.out.println(test2.push(5));
+	System.out.println(test2.push(6));
+	System.out.println(test2.push(7));
+	System.out.println(test2);
+	System.out.println(test2.pop());
+	System.out.println(test2);
+	System.out.println(test2.peek());
+	System.out.println(test2);
 
     }
 
