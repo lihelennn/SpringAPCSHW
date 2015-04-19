5ec7e6478fe912c3b2a9aa21327dc1712fda7cf1
lihelennn <helen82198@gmail.com>
2015-04-18 22:23:21 -0400

    wokring on some index out of bounds exception

--- 11BFS/Driver.java
+++ 11BFS/Driver.java
@@ -3,18 +3,24 @@
 public class Driver{
 
     public static void main(String[]args){
+	
+     	// Maze test = new Maze("data1.dat");
+	// System.out.println(test.name());
+	// System.out.println(test.solveBFS(true));
+	// // System.out.println(test.solveDFS(true));
+	// System.out.println(test.solutionCoordinates());
 
-     	Maze test = new Maze("data1.dat");
-	System.out.println(test.name());
-	System.out.println(test.solveBFS(true));
+	Maze test3 = new Maze("data1.dat");
+	System.out.println(test3.name());
+	System.out.println(test3.solveBest(true));
 	// System.out.println(test.solveDFS(true));
-	System.out.println(test.solutionCoordinates());
+	System.out.println(test3.solutionCoordinates());
 
 
-    	Maze test2 = new Maze("data2.dat");
-	System.out.println(test2.solveBFS(true));
-	// System.out.println(test2.solveDFS(true));
-	System.out.println(test2.solutionCoordinates());
+    	// Maze test2 = new Maze("data2.dat");
+	// System.out.println(test2.solveBFS(true));
+	// // System.out.println(test2.solveDFS(true));
+	// System.out.println(test2.solutionCoordinates());
 
 
     }
--- 11BFS/MyDeque.java
+++ 11BFS/MyDeque.java
@@ -215,22 +215,26 @@ public class MyDeque<T>{
     }
 
     public void add(Object o, int n){
+	System.out.println(nums.length);
+	System.out.println(tail);
 	if (begin2){
 	    nums[tail] = n;
 	    begin2 = false;
 	}else{
+	    if (numElements == size){
+		this.resize(size*2);
+	    }
 	    if (tail + 1 == nums.length){
 		System.out.println(1);
 		tail = 0;
-		System.out.println(tail);
 		nums[tail] = n;
-		System.out.println(tail);
 	    }else{
 		System.out.println(2);
 		nums[tail+1] = n;
+		tail++;
 	    }
-	    tail++;
 	}
+	numElements++;
 	this.addLast((T)o);
     }
 
