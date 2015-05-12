b30f254eac5fd43d50a46edc1bcb47c232f81000
Helen Li <helen82198@gmail.com>
2015-05-12 12:07:49 -0400

    add kind of works

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MyHeap{
 
     private boolean isMax = true;
@@ -11,25 +13,26 @@ public class MyHeap{
     }
 
     private void resize(){
-	int[]sub = new int[arr[0] * 2];
+	int[]sub = new int[capacity * 2];
 	int place = 0;
 	while (place < arr.length){
 	    sub[place] = arr[place];
 	    place++;
 	}
 	arr = sub;
+	System.out.println(Arrays.toString(arr));
 	capacity*=2;
     }
 
 
     public void add(int n){
+	if (arr[0] == capacity-1){
+	    resize();
+	}
 	if (arr[0] == 0){
 	    arr[0]++;
 	    arr[1] = n;
 	}else{
-	    if (arr[0] == capacity){
-		resize();
-	    }
 	    arr[arr[0] + 1] = n;
 	    arr[0]++;
 	    switchParent(arr[0]);
@@ -39,14 +42,33 @@ public class MyHeap{
 
     public void switchParent(int ind){
 	int sub;
-	while ((isMax && arr[ind] > arr[ind/2]) || (!isMax && arr[ind] < arr[ind/2])){
+	while (ind/2 != 0 && (isMax && arr[ind] > arr[ind/2]) || (!isMax && arr[ind] < arr[ind/2])){
 	    sub = arr[ind];
 	    arr[ind] = arr[ind/2];
 	    arr[ind/2] = sub;
 	    ind/=2;
 	}
     }
-	 
 
+    public String toString(){
+	String ans = "[";
+	int place = 0;
+	while (place < arr.length){
+	    ans+=arr[place] + "  ";
+	    place++;
+	}
+	ans+="]";
+	return ans;
+    }
+	 
+    public static void main(String[]args){
+	MyHeap test = new MyHeap();
+	test.add(12);
+	test.add(10);
+	test.add(13);
+	test.add(20);
+	test.add(17);
+	System.out.println(test);
+    }
 
 }
