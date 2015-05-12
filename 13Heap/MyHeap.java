4de838993b570707f1621ce225b6f33ff912d580
lihelennn <helen82198@gmail.com>
2015-05-11 23:55:40 -0400

    compiles lol will test tomorrow in class

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -2,6 +2,7 @@ public class MyHeap{
 
     private boolean isMax = true;
     private int[]arr = new int[5];
+    int capacity = 0;
 
 
     public MyHeap(){
@@ -9,11 +10,26 @@ public class MyHeap{
 	arr[0] = 0;
     }
 
+    private void resize(){
+	int[]sub = new int[arr[0] * 2];
+	int place = 0;
+	while (place < arr.length){
+	    sub[place] = arr[place];
+	    place++;
+	}
+	arr = sub;
+	capacity*=2;
+    }
+
+
     public void add(int n){
 	if (arr[0] == 0){
 	    arr[0]++;
 	    arr[1] = n;
 	}else{
+	    if (arr[0] == capacity){
+		resize();
+	    }
 	    arr[arr[0] + 1] = n;
 	    arr[0]++;
 	    switchParent(arr[0]);
