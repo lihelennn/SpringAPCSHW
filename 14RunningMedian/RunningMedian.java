68b393aede7a98ca49a96b5f796f74e944bd72ca
lihelennn <helen82198@gmail.com>
2015-05-18 01:19:04 -0400

    works for the beginning numbers

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class RunningMedian{
 
     private int med1;
-    private MyHeap min = new MyHeap(false);
-    private MyHeap max = new MyHeap();
+    private MyHeap min = new MyHeap();
+    private MyHeap max = new MyHeap(false);
     private int size1 = 0;
     private int size2 = 0;
 
@@ -24,6 +24,7 @@ public class RunningMedian{
     public void add(int value){
 	if (size1 == 0 && size2 == 0){
 	    min.add(value);
+	    size1++;
 	}else{
 	    if (size2 == 0 && size1 == 1){
 		if (min.peek() < value){
@@ -37,15 +38,55 @@ public class RunningMedian{
 		//if there are two medians
 		if (size1 == size2){
 		    if (min.peek() >= value){
-			max.add(min.remove());
-			size2++;
 			min.add(value);
+			size1++;
 		    }else{
 			if (value >= min.peek() && value < max.peek()){
 			    min.add(value);
 			    size1++;
 			}else{
-			    if (max.peek() <= peek){
+			    if (max.peek() <= value){
+				max.add(value);
+				size2++;
 
+			    }
+			}
+		    }
+		}else{
+		    if (size1 > size2){
+			if (min.peek() <= value){
+			    max.add(value);
+			    size2++;
+			}else{
+			    if (min.peek() >= value){
+				max.add(min.remove());
+				size2++;
+			    }
+			}
+		    }else{
+			if (max.peek() >= value){
+			    min.add(value);
+			    size1++;
+			}else{
+			    if (max.peek() <= value){
+				min.add(max.remove());
+				size1++;
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
 
+    public static void main(String[]args){
+	RunningMedian test = new RunningMedian();
+	test.add(3);
+	test.add(10);
+	test.add(20);
+	test.add(15);
+	System.out.println(test.getMedian());
+    }
+
+}
 		 
